d5e6fe27c47a780522bb876184a577b584867931
Manoli Sergiu <sergiu_manole@hotmail.com>
2023-05-02 15:01:23 +0300

    finished employees db

--- employees-db/src/index.js
+++ employees-db/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/app/App';
+
+import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
